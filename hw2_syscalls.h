4081cc4cf569a32088ff9a6e0ec2c7d0f06b68d3
Eylon <eylontol@gmail.com>
2017-05-03 18:41:51 +0300

    sys calls header
    might be wrong;
    243
    244
    255
    by entry.s

---
+++ hw2_syscalls.h
@@ -0,0 +1,52 @@
+
+
+#ifndef hw2_syscalls_h
+#define hw2_syscalls_h
+
+int is_short(pid_t pid) {
+    unsigned int res;
+    __asm__ (
+             "int $0x80;"
+             : "=a" (res)
+             : "0" (243), "b" (pid)
+             : "memory"
+             );
+    if (res >= (unsigned long)(- 125)) {
+        errno = -res;
+        res = - 1;
+    }
+    return   (int)  res ;
+}
+
+int short_remaining_time(pid_t pid)   {
+    unsigned int res;
+    __asm__ (
+             "int $0x80;"
+             : "=a" (res)
+             : "0" (244), "b" (pid)
+             : "memory"
+             );
+    if (res >= (unsigned long)(- 125)) {
+        errno = -res;
+        res = - 1;
+    }
+    return   (int)  res ;
+}
+
+int short_place_in_queue(pid_t pid)   {
+    unsigned int res;
+    __asm__ (
+             "int $0x80;"
+             : "=a" (res)
+             : "0" (245), "b" (pid)
+             : "memory"
+             );
+    if (res >= (unsigned long)(- 125)) {
+        errno = -res;
+        res = - 1;
+    }
+    return   (int) res ;
+}
+
+
+#endif /* hw2_syscalls_h */
