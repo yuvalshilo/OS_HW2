c71965d1aecdebe182100b7035a5e8834382414c
Eylon <eylontol@gmail.com>
2017-05-09 16:48:58 +0300

    aaa

--- hw2_syscalls.c
+++ hw2_syscalls.c
@@ -9,3 +9,15 @@ int sys_is_short(pid_t pid) {
     }
     return  (ts->is_overdue == OVERDUE ? 0 : 1);
 }
+
+int sys_short_remaining_time(pid_t pid) {
+    struct task_t* ts = find_task_by_pid(pid);
+    if (!ts) {
+        return -ESRCH;
+    }
+    if (ts->policy != SCHED_SHORT) {
+        return -EINVAL
+    }
+
+    return ts->time_slice;
+}
--- test.c
+++ test.c
@@ -6,4 +6,9 @@
 #include <sys/types.h>
 #include "hw2_syscalls.h"
 
-
+int main() {
+    
+    
+    
+    return 0;
+}
